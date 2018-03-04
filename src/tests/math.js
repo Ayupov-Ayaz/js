b26fc25e31b99b483e020b4faaf5a12df9379380
Аюпов Аяз <ayaz.ayupov@gmail.com>
2018-03-04 15:06:22 +0300

    before, after, beforeEach, afterEach in testing

--- src/tests/math.js
+++ src/tests/math.js
@@ -1,14 +1,22 @@
 
 var assert = chai.assert;
-
-describe("pow", function(){
+before(function() {console.log('Начало тестов')});
+after(function() {console.log('Конец тестов')});
+  
+describe("pow", function() {
+  beforeEach(function() { console.log('Запуск теста pow')});
+  afterEach(function() { console.log('Выход из теста pow')});
+  
   it("Возводит в степень",
     function(){
       assert.equal(pow(2,3), 8);
     })
 });
 
-describe("fibo", function(){
+describe("fibo", function() {
+  beforeEach(function() { console.log('Запуск теста fibo')});
+  afterEach(function() {console.log('Выход из теста fibo')});
+  
   it("Вычисление суммы чисел фибоначи",
      function(){
       assert.equal(fibo(7), 13);
