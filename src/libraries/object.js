afffd76f345c038b8a9d5a6e9d26a326656e765f
Аюпов Аяз <ayaz.ayupov@gmail.com>
2018-03-11 17:03:28 +0300

    + removeClass in object

--- src/libraries/object.js
+++ src/libraries/object.js
@@ -16,3 +16,29 @@ function addClass(obj, propertyName, className, separator = " ") {
   classes.push(className);
   obj[propertyName] =  classes.join(' ');
 }
+
+/**
+ * Функция для удаления класса из свойств объекта
+ * @param {Object} obj 
+ * @param {string} propertyName  свойство объекта
+ * @param {string} className  название класса, который нужно удалить
+ * @param {string} separator  разделитель строки с классом для функции split 
+ */
+function removeClass(obj, propertyName, className, separator = ' ') {
+  var classes = obj[propertyName].split(separator);
+  
+  var changed = false;
+  for(var i = 0; i < classes.length;) {
+    if(classes[i] == className) {
+      classes.splice(i, 1);
+      changed = true;
+    }else{
+      i++;
+    }
+  }
+   
+  if(changed) {
+    var strClasses = classes.join(' ');
+    obj[propertyName] = strClasses;
+  }
+}
