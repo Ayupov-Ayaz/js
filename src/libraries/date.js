e81242fce2cb21fd79612b23cce95eb94e2ec1b9
Аюпов Аяз <ayaz.ayupov@gmail.com>
2018-08-09 21:58:05 +0300

    added date functions

--- src/libraries/date.js
+++ src/libraries/date.js
@@ -10,3 +10,29 @@ function getDateAgo(date, days){
     var newDate = currentDate - lastDate;
     alert(new Date(newDate));
 }
+
+/**
+ * Функция по получению дня недели из даты
+ * @param date
+ * @returns {string}
+ */
+function getWeekDay(date) {
+    var days = getWeek();
+    return days[date.getDay()];
+}
+
+/**
+ * Получить форматы дней недель
+ * @param format - формат
+ * @param location - язык
+ * @returns {string[]}
+ */
+function getWeek(format, location) {
+    format = format || 'sm';
+    location = location || 'ru';
+    if(format === 'sm' && location === 'ru') {
+        return ['вс','пн','вт','ср','чт','пт','суб'];
+    } else if(format === 'la' && location === 'ru') {
+        return ['воскресенье','понедельник','вторник','среда','четверг','пятница','суббота'];
+    }
+}
